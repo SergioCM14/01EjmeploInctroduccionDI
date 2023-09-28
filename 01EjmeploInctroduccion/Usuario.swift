d8b60be1a5780d836884ebae9c89cdabb787def4
seriocatalanmunoz <sergio.catalan.munoz@cieep.com>
2023-09-28 14:24:37 +0200

    ejemplo casi terminado, falta TOAST

---
+++ 01EjmeploInctroduccion/Usuario.swift
@@ -0,0 +1,13 @@
+//
+//  Usuario.swift
+//  01EjmeploInctroduccion
+//
+//  Created by Serio Catalán Muñoz on 28/09/2023.
+//
+
+import Foundation
+
+struct Usuario {
+    var email: String
+    var password: String
+}
--- 01EjmeploInctroduccion/ViewController.swift
+++ 01EjmeploInctroduccion/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var lbResultado: UILabel!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
     var contador: UInt!
+    var listaUsuarios: [Usuario]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         contador = 0
+        listaUsuarios = []
     }
 
     @IBAction func cuentaClicks(_ sender: Any) {
         contador += 1
         lbResultado.text = "\(String (contador)) veces"// la contrabarra (\) es para concatenar
+    }
+    
+    @IBAction func RegistrarUsuario(_ sender: Any) {
+        let email: String = txtEmail.text!
+        let password: String = txtPassword.text!
+        let Usuario:Usuario = Usuario(email:email, password:password)
+        listaUsuarios.append(Usuario)
+        print("Email: \(email) Password: \(password)")
     }
     
 }
