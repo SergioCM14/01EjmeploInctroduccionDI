93df9c9e328c189f986de2e8be4b61fedcd98a8e
seriocatalanmunoz <sergio.catalan.munoz@cieep.com>
2023-09-28 13:52:23 +0200

    Ejemplo contadorClicks finalizado

--- 01EjmeploInctroduccion/ViewController.swift
+++ 01EjmeploInctroduccion/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbResultado: UILabel!
+    var contador: UInt!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        contador = 0
     }
 
-
+    @IBAction func cuentaClicks(_ sender: Any) {
+        contador += 1
+        lbResultado.text = "\(String (contador)) veces"// la contrabarra (\) es para concatenar
+    }
+    
 }
 
